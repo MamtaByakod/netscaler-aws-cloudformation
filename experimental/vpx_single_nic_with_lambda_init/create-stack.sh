ffcf5b919ead139c63802d8b4c3e9b3b14bdf787
Chiradeep Vittal <chiradeepv@gmail.com>
2017-11-21 06:13:15 +0000

    Add the ability to specify a CLI script as an input param

--- experimental/vpx_single_nic_with_lambda_init/create-stack.sh
+++ experimental/vpx_single_nic_with_lambda_init/create-stack.sh
@@ -3,3 +3,7 @@
 
 # Sample invocation to create a single NIC VPX. You HAVE to customize ParameterValues to your environment
 aws cloudformation create-stack --template-body file://ns.1nic.template --capabilities CAPABILITY_IAM --stack-name vpx-1nic-test --parameters  ParameterKey=Subnet,ParameterValue=subnet-a1b2c3d4 ParameterKey=VpcID,ParameterValue=vpc-1a2b3c4d ParameterKey=ImageName,ParameterValue=VpxStandard1000 ParameterKey=KeyName,ParameterValue=mykeyname
+
+
+aws --region=us-west-2 cloudformation create-stack --template-body file://ns.1nic_init_script.template --stack-name vpx-1nic-test2 --parameters  file://params.json --capabilities CAPABILITY_IAM
+
